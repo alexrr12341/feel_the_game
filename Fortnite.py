874b61232acb57fa2da732bfa1b809ee0c24a83f
Alejandro Rodríguez Rojas <alexrodriguezrojas98@gmail.com>
2019-05-16 23:11:03 +0200

    Modificado fichero fortnite

--- Fortnite.py
+++ Fortnite.py
@@ -1,36 +1,52 @@
 import requests
 import os
-plat=str(input("¿Cual es tu plataforma? (pc,xbl,psn) "))
-jugador=str(input("¿Cual es el nombre de jugador? "))
 key=os.environ["keyf"]
 header={'TRN-Api-Key':key}
 URL_BASE='https://api.fortnitetracker.com/v1/'
-r=requests.get(URL_BASE+'profile/%s/%s'%(plat,jugador),headers=header)
-try:
-    datos=r.json()
-    lista=['top1','top10','top25','winRatio','matches','score','kills','kd']
-    print('Plataforma:',datos['platformNameLong'])
-    print('Informacion de Solo')
-    id=datos['accountId']
-    for infoSolo in lista:
-        print(datos["stats"]["p2"][infoSolo]["label"],':',datos["stats"]["p2"][infoSolo]["value"])
-    print('Informacion de Duo')
-    for infoDuos in lista:
-        print(datos["stats"]["p10"][infoDuos]["label"],':',datos["stats"]["p10"][infoDuos]["value"])
-    print('Informacion de Squad')
-    for infoSquad in lista:
-        print(datos["stats"]["p9"][infoSquad]["label"],':',datos["stats"]["p9"][infoSquad]["value"])
-    print('Informacion General')
-    for infoGeneral in datos['lifeTimeStats']:
-        print(infoGeneral['key'],':',infoGeneral['value'])
-    URL_Historial='https://api.fortnitetracker.com'+'/v1/store'
-    t=requests.get(URL_Historial,headers=header)
+def tienda_fortnite():
+    t=requests.get(URL_BASE+'store',headers=header)
     datos2=t.json()
+    tiendaf={}
+    listaf=[]
     for tienda in datos2:
         print('Nombre:',tienda['name'])
         print('Rareza',tienda['rarity'])
         print('vBucks',tienda['vBucks'])
         print('Imagen',tienda['imageUrl'])
-except:
-    print('Ese usuario no existe')
-    
+        tiendaf['Nombre']=tienda['name'] 
+        tiendaf['Rareza']=tienda['rarity']
+        tiendaf['VBucks']=tienda['vBucks']
+        tiendaf['Imagen']=tienda['imageUrl']
+        listaf.append(tiendaf)
+    return listaf
+def estadisticas_fortnite(plat,jugador):
+    r=requests.get(URL_BASE+'profile/%s/%s'%(plat,jugador),headers=header)
+    datos=r.json()
+    lista=['top1','top10','top25','winRatio','matches','score','kills','kd']
+    listasolo=['Solo']
+    id=datos['accountId']
+    tiendag={}
+    tiendasq={}
+    tiendadu={}
+    tiendaso={}
+    tiendaso2={}
+    tiendadu2={}
+    tiendasq2={}
+    tiendag2={}
+    for infoSolo in lista:
+        tiendaso[datos['stats']['p2'][infoSolo]['label']]=datos['stats']['p2'][infoSolo]['value']
+    tiendaso2['Solo']=tiendaso
+    for infoDuos in lista:
+        tiendadu[datos['stats']['p10'][infoDuos]['label']]=datos['stats']['p10'][infoDuos]['value']
+    tiendadu2['Duo']=tiendadu
+    for infoSquad in lista:
+        tiendasq[datos['stats']['p9'][infoSquad]['label']]=datos['stats']['p9'][infoSquad]['value']
+    tiendasq2['Squad']=tiendasq
+    for infoGeneral in datos['lifeTimeStats']:
+        tiendag[infoGeneral['key']]=infoGeneral['value']
+    tiendag2['General']=tiendag
+    return tiendag2,tiendasq2,tiendadu2,tiendaso2
+#0 = General
+#1 = Squad
+#2 = Duo
+#3 = Solo

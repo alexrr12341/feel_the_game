1c112050188f4078a8c67be8466aa4408ef44bb7
Alejandro Rodríguez Rojas <alexrodriguezrojas98@gmail.com>
2019-05-17 14:44:19 +0200

    Mejorado eSport

--- eSport.py
+++ eSport.py
@@ -3,63 +3,79 @@ import requests
 key=os.environ['keypanda']
 parametros={'token':key}
 URL_BASE='https://api.pandascore.co'
-r=requests.get(URL_BASE+'//lol/tournaments/running',params=parametros)
-datos=r.json()
 diccequipos={}
 dicctorneo={}
 def sacar_torneo():
-        for torneos in datos:
-                torneo=torneos['league']['name'].capitalize()
-                imagen=torneos['league']['image_url']
-                dicctorneo['Nombre']=torneo
-                dicctorneo['Imagen']=imagen
-        return dicctorneo
+        r=requests.get(URL_BASE+'//lol/tournaments/running',params=parametros)
+        datos=r.json()
+        if r.status_code==200:
+                for torneos in datos:
+                        torneo=torneos['league']['name'].capitalize()
+                        imagen=torneos['league']['image_url']
+                        dicctorneo['Nombre']=torneo
+                        dicctorneo['Imagen']=imagen
+                return dicctorneo
+        else:
+                print('Error de Api')
 def conseguir_enfrentamientos():
+        r=requests.get(URL_BASE+'//lol/tournaments/running',params=parametros)
+        datos=r.json()
         diccequiposT={}
         listaequipos=[]
         diccequipos2={}
-        for torneos in datos:
-                for equipos in torneos['teams']:
-                        id=equipos['id']
-                        id2=str(id)
-                        diccequipos[id2]=equipos['name']
-                for partidas in torneos['matches']:
-                        Ganador=partidas['winner_id']
-                        Ganador2=str(Ganador)
-                        diccequiposT['Empieza']=partidas['begin_at']
-                        diccequiposT['Termina']=partidas['end_at']
-                        diccequiposT['Tipo']=partidas['match_type']
-                        diccequiposT['Enfrentamiento']=partidas['name']
-                        diccequiposT['Ganador']=diccequipos[Ganador2]
-                        listaequipos.append(diccequiposT.copy())
-        return listaequipos
-t=requests.get(URL_BASE+'//lives',params=parametros)
-datos2=t.json()
+        if r.status_code==200:
+                for torneos in datos:
+                        for equipos in torneos['teams']:
+                                id=equipos['id']
+                                id2=str(id)
+                                diccequipos[id2]=equipos['name']
+                        for partidas in torneos['matches']:
+                                Ganador=partidas['winner_id']
+                                Ganador2=str(Ganador)
+                                diccequiposT['Empieza']=partidas['begin_at']
+                                diccequiposT['Termina']=partidas['end_at']
+                                diccequiposT['Tipo']=partidas['match_type']
+                                diccequiposT['Enfrentamiento']=partidas['name']
+                                diccequiposT['Ganador']=diccequipos[Ganador2]
+                                listaequipos.append(diccequiposT.copy())
+                return listaequipos
+        else:
+                print('Error de Api')
 def obtener_enfrentamientos_lives():
+        t=requests.get(URL_BASE+'//lives',params=parametros)
+        datos2=t.json()
         dicclol={}
         listalol=[]
-        for torneosdirecto in datos2:
-                if torneosdirecto['event']['game']=='league-of-legends':
-                        dicclol['URLStream']=torneosdirecto['event']['stream_url']
-                        dicclol['Juego']=torneosdirecto['event']['game']
-                        dicclol['id']=torneosdirecto['event']['id']
-                        dicclol['Tid']=torneosdirecto['event']['tournament_id']
-                        listalol.append(dicclol.copy())
-        return listalol
+        if t.status_code==200:
+                for torneosdirecto in datos2:
+                        if torneosdirecto['event']['game']=='league-of-legends':
+                                dicclol['URLStream']=torneosdirecto['event']['stream_url']
+                                dicclol['Juego']=torneosdirecto['event']['game']
+                                dicclol['id']=torneosdirecto['event']['id']
+                                dicclol['Tid']=torneosdirecto['event']['tournament_id']
+                                listalol.append(dicclol.copy())
+                return listalol
+        else:
+                print('Error de Api')
 def obtener_match_lives():
+        t=requests.get(URL_BASE+'//lives',params=parametros)
+        datos2=t.json()
         diccmatch={}
         listamatch=[]
         listaids=[]
         diccenfrentamiento={}
-        for identificador in obtener_enfrentamientos_lives():
-                listaids.append(identificador['Tid'])
-        for ids in listaids:
-                for matches in datos2:
-                        if matches['match']['tournament']['id']==ids:
-                                diccmatch['Termina']=matches['match']['tournament']['end_at']
-                                diccmatch['Nombre']=matches['match']['tournament']['slug'].capitalize()
-                                diccmatch['NombreMatch']=matches['match']['name']
-                                listamatch.append(diccmatch.copy())
-        return listamatch
+        if t.status_code==200:
+                for identificador in obtener_enfrentamientos_lives():
+                        listaids.append(identificador['Tid'])
+                for ids in listaids:
+                        for matches in datos2:
+                                if matches['match']['tournament']['id']==ids:
+                                        diccmatch['Termina']=matches['match']['tournament']['end_at']
+                                        diccmatch['Nombre']=matches['match']['tournament']['slug'].capitalize()
+                                        diccmatch['NombreMatch']=matches['match']['name']
+                                        listamatch.append(diccmatch.copy())
+                return listamatch
+        else:
+                print('Error de Api')
 #league-of-legends
 

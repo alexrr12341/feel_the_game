749786c71ee01eb678b871ee83aeec6ff9b22139
Alejandro Rodríguez Rojas <alexrodriguezrojas98@gmail.com>
2019-05-17 11:51:17 +0200

    Modificado eSports

--- eSport.py
+++ eSport.py
@@ -6,23 +6,35 @@ URL_BASE='https://api.pandascore.co'
 r=requests.get(URL_BASE+'//lol/tournaments/running',params=parametros)
 datos=r.json()
 diccequipos={}
-for torneos in datos:
-    for equipos in torneos['teams']:
-        print(equipos['name'],equipos['id'])
-        id=equipos['id']
-        id2=str(id)
-        diccequipos[id2]=equipos['name']
-    for partidas in torneos['matches']:
-        print('Empieza a las:',partidas['begin_at'])
-        print('Termina a las:',partidas['end_at'])
-        print('Tipo de partido',partidas['match_type'])
-        print('Enfrentamiento',partidas['name'])
-        Ganador=partidas['winner_id']
-        Ganador2=str(Ganador)
-        print('Ganador:',diccequipos[Ganador2])
-
+dicctorneo={}
+print(datos)
+def sacar_torneo():
+        for torneos in datos:
+                torneo=torneos['league']['name'].capitalize()
+                imagen=torneos['league']['image_url']
+                dicctorneo['Nombre']=torneo
+                dicctorneo['Imagen']=imagen
+        return dicctorneo
+def conseguir_enfrentamientos():
+        diccequiposT={}
+        listaequipos=[]
+        diccequipos2={}
+        for torneos in datos:
+                for equipos in torneos['teams']:
+                        id=equipos['id']
+                        id2=str(id)
+                        diccequipos[id2]=equipos['name']
+                for partidas in torneos['matches']:
+                        Ganador=partidas['winner_id']
+                        Ganador2=str(Ganador)
+                        diccequiposT['Empieza']=partidas['begin_at']
+                        diccequiposT['Termina']=partidas['end_at']
+                        diccequiposT['Tipo']=partidas['match_type']
+                        diccequiposT['Enfrentamiento']=partidas['name']
+                        diccequiposT['Ganador']=diccequipos[Ganador2]
+                        listaequipos.append(diccequiposT.copy())
+        return listaequipos
 t=requests.get(URL_BASE+'//lives',params=parametros)
 datos2=t.json()
 for torneosdirecto in datos2:
-    for directos in torneosdirecto['match']:
-        print(directos['games'])
+    print('Hola')

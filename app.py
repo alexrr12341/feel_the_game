13cb9e41b2bffca3e98ec301fc4043dfdbd57ae1
alejandro <alexrodriguezrojas98@gmail.com>
2019-05-27 14:19:20 +0200

    twitter

--- app.py
+++ app.py
@@ -96,13 +96,10 @@ def get_request_token_oauth1():
 	return credentials.get(b'oauth_token')[0]
 
 def get_access_token_oauth1(request_token,verifier):
-	oauth = OAuth1(CONSUMER_KEY,
-					client_secret=CONSUMER_SECRET,
-					resource_owner_key=request_token,
-					verifier=verifier,)
-	r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
-	credentials = parse_qs(r.content)
-	session["screen_name"] = credentials.get(b'screen_name')[0]
+    oauth = OAuth1(CONSUMER_KEY,client_secret=CONSUMER_SECRET,resource_owner_key=request_token,verifier=verifier)
+    r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    session["screen_name"] = credentials.get(b'screen_name')[0]
     return credentials.get(b'oauth_token')[0],credentials.get(b'oauth_token_secret')[0]
 @app.route('/twitter',methods=['POST','GET'])
 def twitter():

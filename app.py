1819950792767147fd76dec2664c46a3c826eaf4
alejandro <alexrodriguezrojas98@gmail.com>
2019-05-28 12:03:24 +0200

    Añadiendo twitter a fortnite

--- app.py
+++ app.py
@@ -141,6 +141,37 @@ def twittear():
     r=requests.post(UPDATE_URL, data=post, auth=oauth)
     if r.status_code==200:
         return redirect('https://twitter.com/')
+@app.route('/twitter2)
+def twitter2():
+    request_token,request_token_secret = get_request_token_oauth1()
+    authorize_url = AUTHENTICATE_URL + request_token.decode("utf-8")
+    session["request_token"]=request_token.decode("utf-8")
+    session["request_token_secret"]=request_token_secret.decode("utf-8")
+    return redirect(authorize_url)
+@app.route('/twitter_callback')
+def twitter_callback2():
+    request_token=session["request_token"]
+    request_token_secret=session["request_token_secret"]
+    verifier  = request.args.get("oauth_verifier")
+    access_token,access_token_secret= get_access_token_oauth1(request_token,request_token_secret,verifier)
+    session["access_token"]= access_token.decode("utf-8")
+    session["access_token_secret"]= access_token_secret.decode("utf-8")
+    return redirect('/twittear2')
+@app.route('/twittear2')
+def twittear2():
+    invocador=session['invocador']
+    liga=session['liga']
+    victorias=session['victorias']
+    update = '''Hola, mi nombre en el LeagueOfLegends es %s soy %s y tengo %s victorias de rankeds.
+				Mira tus estadísticas en:
+				https://feelthegame.herokuapp.com/'''%(invocador,liga,victorias)
+    post = {"status": update}
+    access_token=session["access_token"]
+    access_token_secret=session["access_token_secret"]
+    oauth = OAuth1(CONSUMER_KEY,client_secret=CONSUMER_SECRET,resource_owner_key=access_token,resource_owner_secret=access_token_secret)
+    r=requests.post(UPDATE_URL, data=post, auth=oauth)
+    if r.status_code==200:
+        return redirect('https://twitter.com/')
 if __name__ == '__main__':
     port=os.environ["PORT"]
 app.run('0.0.0.0',int(port), debug=True)
